474f680694688272e04acd52e9732a3983ffe1b4
Jason Fieldman <jason@fieldman.org>
2013-02-13 18:01:22 -0800

    moved files for new project

---
+++ HarlemShake/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HarlemShake
+//
+//  Created by Jason Fieldman on 2/13/13.
+//  Copyright (c) 2013 Jason Fieldman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HarlemShake/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HarlemShake
+//
+//  Created by Jason Fieldman on 2/13/13.
+//  Copyright (c) 2013 Jason Fieldman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
