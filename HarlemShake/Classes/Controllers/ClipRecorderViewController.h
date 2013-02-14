3627c554d290282e57d623cc820d8fe5fe7d8140
Jason Fieldman <jason@fieldman.org>
2013-02-13 23:05:18 -0800

    orientation tracking

--- HarlemShake/Classes/Controllers/ClipRecorderViewController.h
+++ HarlemShake/Classes/Controllers/ClipRecorderViewController.h
@@ -9,12 +9,26 @@
 #import <Foundation/Foundation.h>
 
 @interface ClipRecorderViewController : UIViewController <AVCaptureFileOutputRecordingDelegate> {
+	
+	float _screenH;
+	float _screenW;
+	
+	UIView *_interfaceContainer;
+	
+	UIView *_topRContainer;
+	UIView *_topLContainer;
+	UIView *_botLContainer;
+	
 	UIButton *_cancelButton;
 	
 	/* Recording */
 	AVCaptureSession     *_captureSession;
 	AVCaptureDevice      *_captureDevice;
 	AVCaptureDeviceInput *_captureDeviceInput;
+	
+	/* Orientation tracking */
+	CMMotionManager    *_motionManager;
+	UIDeviceOrientation _currentOrientation;
 }
 
 @property (nonatomic, strong) VideoID_t videoId;
--- HarlemShake/Classes/Controllers/ClipRecorderViewController.m
+++ HarlemShake/Classes/Controllers/ClipRecorderViewController.m
@@ -34,9 +34,24 @@
 		captureVideoPreviewLayer.backgroundColor = [UIColor blackColor].CGColor;
 		[self.view.layer addSublayer:captureVideoPreviewLayer];
 				 
+		/* Create containers */
+		_screenW = self.view.frame.size.width;
+		_screenH = self.view.frame.size.height;
+		_interfaceContainer = [[UIView alloc] initWithFrame:CGRectMake(- (_screenH - _screenW) / 2, 0, _screenH, _screenH)];
+		_interfaceContainer.backgroundColor = [UIColor clearColor];
+		[self.view addSubview:_interfaceContainer];
+		
+		_topLContainer = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 160, 160)];
+		_topLContainer.backgroundColor = [UIColor clearColor];
+		[_interfaceContainer addSubview:_topLContainer];
+		
+		/* Set interface to portrait */
+		[self positionSubcontainersForPortrait];
+		_currentOrientation = UIDeviceOrientationPortrait;
+		
 		/* Cancel */
 		_cancelButton = [UIButton buttonWithType:UIButtonTypeCustom];
-		_cancelButton.frame = CGRectMake(150,150,100,50);
+		_cancelButton.frame = CGRectMake(50,10,100,50);
 		_cancelButton.backgroundColor = [UIColor whiteColor];
 		[_cancelButton setTitle:@"Cancel" forState:UIControlStateNormal];
 		[_cancelButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
@@ -47,8 +62,11 @@
 		_cancelButton.layer.borderWidth = 5;
 		_cancelButton.layer.borderColor	= [UIColor blackColor].CGColor;
 		_cancelButton.alpha = 0.75;
-		[self.view addSubview:_cancelButton];
+		[_topLContainer addSubview:_cancelButton];
 				
+		/* Motion */
+		_motionManager = [[CMMotionManager alloc] init];
+		_motionManager.accelerometerUpdateInterval = 0.5;
 	}
 	return self;
 }
@@ -56,9 +74,36 @@
 - (void) viewWillAppear:(BOOL)animated {
 	[super viewWillAppear:animated];
 	[[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationFade];
+	
+	[_motionManager startAccelerometerUpdatesToQueue:[NSOperationQueue currentQueue]
+										 withHandler:^(CMAccelerometerData *accData, NSError *error) {
+
+											 /* 
+											  port: y = -1
+											  upside down port: y = 1
+											  home button right: x = -1
+											  home button left: x = 1
+											  */
+											 
+											 UIDeviceOrientation curOrientation = UIDeviceOrientationUnknown;
+											 
+											 if (accData.acceleration.x > 0.7)  curOrientation = UIDeviceOrientationLandscapeRight;
+											 if (accData.acceleration.x < -0.7) curOrientation = UIDeviceOrientationLandscapeLeft;
+											 if (accData.acceleration.y > 0.7)  curOrientation = UIDeviceOrientationPortraitUpsideDown;
+											 if (accData.acceleration.y < -0.7) curOrientation = UIDeviceOrientationPortrait;
+											 
+											 if (curOrientation == UIDeviceOrientationUnknown) return;
+											 if (curOrientation == _currentOrientation) return;
+											 
+											 [self positionContainerForOrientation:curOrientation];
+											 
+											 
+	}];
 }
 
 - (void) viewWillDisappear:(BOOL)animated {
+	[_motionManager stopAccelerometerUpdates];
+	
 	[super viewWillDisappear:animated];
 	[[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationFade];
 }
@@ -66,6 +111,47 @@
 - (void) pressedCancel:(id)sender {
 	[_captureSession stopRunning];
 	[self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+/* Position subcontainers */
+
+- (void) positionContainerForOrientation:(UIDeviceOrientation)orientation {
+	_currentOrientation = orientation;
+	
+	[UIView beginAnimations:nil context:NULL];
+	[UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+	[UIView setAnimationDuration:0.25];
+
+	switch (orientation) {
+		case UIDeviceOrientationPortrait:           _interfaceContainer.transform = CGAffineTransformMakeRotation(0);            break;
+		case UIDeviceOrientationPortraitUpsideDown: _interfaceContainer.transform = CGAffineTransformMakeRotation(M_PI);         break;
+		case UIDeviceOrientationLandscapeLeft:      _interfaceContainer.transform = CGAffineTransformMakeRotation(1 * M_PI / 2); break;
+		case UIDeviceOrientationLandscapeRight:     _interfaceContainer.transform = CGAffineTransformMakeRotation(3 * M_PI / 2); break;
+		default: break;
+	}
+	
+	if (UIDeviceOrientationIsPortrait(orientation)) {
+		[self positionSubcontainersForPortrait];
+	} else {
+		[self positionSubcontainersForLandscape];
+	}
+	
+	[UIView commitAnimations];
+	
+	
+}
+
+- (void) positionSubcontainersForPortrait {
+	float leftMargin = (_screenH - _screenW) / 2;
+	
+	_topLContainer.frame = CGRectMake(leftMargin + (_screenW - _topLContainer.frame.size.width), 0, _topLContainer.frame.size.width, _topLContainer.frame.size.height);
+}
+
+- (void) positionSubcontainersForLandscape {
+	float topMargin = (_screenH - _screenW) / 2;
+	
+	_topLContainer.frame = CGRectMake(_screenH - _topLContainer.frame.size.width, topMargin, _topLContainer.frame.size.width, _topLContainer.frame.size.height);
 }
 
 
